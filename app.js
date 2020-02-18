91253c9b7f45b3c8deacc4490847111ea5245a86
Carl Humphreys <humphreys.carl.d@gmail.com>
2020-02-18 20:58:25 +0000

    project setup
    using parcel js

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+
+import App from './src/App.js';
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
